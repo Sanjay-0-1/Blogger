cd0c64e96e5993f70255cee285a7cf62c72a7352
Sanjay-0-1 <b.yajnas0901@gmail.com>
2024-08-03 13:04:26 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blogging_Website
+
+Once you forked this repo. Make sure to run `npm install` command inside the folder to install all the package.
+
+After installing packages. You must go to your firbase console and make a project. Copy project credentials and paste it to "firebase.js" file. Otherwise you'll not be able to access database.
---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyD17Wdh8gxvntLjYZSiy7ltS3HXPkOAy80",
+    authDomain: "fir-530b9.firebaseapp.com",
+    projectId: "fir-530b9",
+    storageBucket: "fir-530b9.appspot.com",
+    messagingSenderId: "758167919330",
+    appId: "1:758167919330:web:d3aa3ba1dcdad28f0a5cbb"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
