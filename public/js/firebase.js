8866b558da5e774ef37b2e78c896b5d081b0a2ad
Sanjay-0-1 <b.yajnas0901@gmail.com>
2024-08-05 10:08:25 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,10 +1,5 @@
 let firebaseConfig = {
-    apiKey: "AIzaSyD17Wdh8gxvntLjYZSiy7ltS3HXPkOAy80",
-    authDomain: "fir-530b9.firebaseapp.com",
-    projectId: "fir-530b9",
-    storageBucket: "fir-530b9.appspot.com",
-    messagingSenderId: "758167919330",
-    appId: "1:758167919330:web:d3aa3ba1dcdad28f0a5cbb"
+  Config from your firebase app
   };
 
   firebase.initializeApp(firebaseConfig);
